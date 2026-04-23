4342a1ba554acc81f45abb6ef32f6bcfa3dc6888
Rishabh-721 <rishabhrajput337@gmail.com>
2026-04-23 08:23:51 +0530

    made header & filter responsive

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <header>
-      <h1>Employee Management System</h1>
+      <h1 className='fullHeading'>Employee Management System</h1>
+      <h1 className='shortHeading'>EMS</h1>
       <button onClick={() => navigate("/create")}>+ Create Employee</button>
     </header>
   )
